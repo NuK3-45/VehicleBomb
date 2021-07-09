d9457489fe06da3788da726a1af13dc9cc78f812
NuK3-45 <67566960+NuK3-45@users.noreply.github.com>
2021-07-08 22:36:33 -0400

    1.1

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,6 +4,7 @@ game "gta5"
 name "Vehicle Bomb"
 description "A simple vehicle bomb script written in LUA."
 author "NuK3"
-version "1.0.0"
+version "1.1"
 
 client_script "client.lua"
+server_script "check.js"
