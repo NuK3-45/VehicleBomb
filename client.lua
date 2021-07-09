61cff9cb77c7f91568a5b5f5a8a143b08da93b08
NuK3-45 <67566960+NuK3-45@users.noreply.github.com>
2021-07-09 11:33:15 -0400

    1.1

--- client.lua
+++ client.lua
@@ -1,6 +1,16 @@
 -- Config --
 local detonateRadius = 175
 
+local LSC = {
+    vector3(-335.5, -134.08, 39.51),
+    vector3(-1154.54, -2009.08, 13.18),
+}
+local Beekers = {
+    vector3(1179.18, 2639.21, 39.35),
+    vector3(106.11, 6624.48, 33.36)
+}
+local Bennys = vector3(-208.78, -1325.7, 30.89)
+
 -- Place Vehicle Bomb --
 RegisterCommand('placecarbomb', function()
 
@@ -9,19 +19,25 @@ RegisterCommand('placecarbomb', function()
     local isPedInVehicle = IsPedInVehicle(getPlayerPed, playerVehicle, 0)
     local playerLoc = GetEntityCoords(getPlayerPed)
 
-    if isPedInVehicle == 1 and GetDistanceBetweenCoords(1179.18, 2639.21, 39.35, playerLoc.x, playerLoc.y, playerLoc.z) < 12 then
-        AddVehiclePhoneExplosiveDevice(playerVehicle)
-        exports.pNotify:SendNotification({text = "You have placed a car bomb.", type = "success", timeout = (10000), layout = "bottomCenter",})
-    elseif isPedInVehicle == 1 and GetDistanceBetweenCoords(106.11, 6624.48, 33.36, playerLoc.x, playerLoc.y, playerLoc.z) < 12 then
-        AddVehiclePhoneExplosiveDevice(playerVehicle)
-        exports.pNotify:SendNotification({text = "You have placed a car bomb.", type = "success", timeout = (10000), layout = "bottomCenter",})
-    elseif isPedInVehicle == 1 and GetDistanceBetweenCoords(-335.5, -134.08, 39.51, playerLoc.x, playerLoc.y, playerLoc.z) < 21 then
-        AddVehiclePhoneExplosiveDevice(playerVehicle)
-        exports.pNotify:SendNotification({text = "You have placed a car bomb.", type = "success", timeout = (10000), layout = "bottomCenter",})
-    elseif isPedInVehicle == 1 and GetDistanceBetweenCoords(-208.78, -1325.7, 30.89, playerLoc.x, playerLoc.y, playerLoc.z) < 15 then
-        AddVehiclePhoneExplosiveDevice(playerVehicle)
-        exports.pNotify:SendNotification({text = "You have placed a car bomb.", type = "success", timeout = (10000), layout = "bottomCenter",})
-    elseif isPedInVehicle == 1 and GetDistanceBetweenCoords(-1154.54, -2009.08, 13.18, playerLoc.x, playerLoc.y, playerLoc.z) < 21 then
+    local inLSC = false
+    local inBeekers = false
+    local inBennys = false
+    
+    for k, v in pairs(LSC) do
+        if #(playerLoc - v) < 21.0 then
+            inLSC = true
+        end
+    end
+    for k, v in pairs(Beekers) do
+        if #(playerLoc - v) < 12.0 then
+            inBeekers = true
+        end
+    end
+    if #(playerLoc - Bennys) < 15.0 then
+        inBennys = true
+    end
+
+    if isPedInVehicle == 1 and inLSC or inBeekers or inBennys then
         AddVehiclePhoneExplosiveDevice(playerVehicle)
         exports.pNotify:SendNotification({text = "You have placed a car bomb.", type = "success", timeout = (10000), layout = "bottomCenter",})
     else
@@ -34,24 +50,29 @@ RegisterCommand('removecarbomb', function()
 
     local getPlayerPed = GetPlayerPed(-1)
     local playerVehicle = GetVehiclePedIsUsing(getPlayerPed) -- get player vehicle
-    local playerLastVehicle = GetVehiclePedIsIn(getPlayerPed, 1)
     local doesCurrentCarHaveBomb = HasVehiclePhoneExplosiveDevice(playerVehicle)
     local isPedInVehicle = IsPedInVehicle(getPlayerPed, playerVehicle, 0)
     local playerLoc = GetEntityCoords(getPlayerPed)
+
+    local inLSC = false
+    local inBeekers = false
+    local inBennys = false
     
-    if doesCurrentCarHaveBomb == 1 and isPedInVehicle == 1 and GetDistanceBetweenCoords(1179.18, 2639.21, 39.35, playerLoc.x, playerLoc.y, playerLoc.z) < 12 then
-        ClearVehiclePhoneExplosiveDevice(playerVehicle)
-        exports.pNotify:SendNotification({text = "Your car bomb is removed.", type = "success", timeout = (10000), layout = "bottomCenter",})
-    elseif doesCurrentCarHaveBomb == 1 and isPedInVehicle == 1 and GetDistanceBetweenCoords(106.11, 6624.48, 33.36, playerLoc.x, playerLoc.y, playerLoc.z) < 12 then
-        ClearVehiclePhoneExplosiveDevice(playerVehicle)
-        exports.pNotify:SendNotification({text = "Your car bomb is removed.", type = "success", timeout = (10000), layout = "bottomCenter",})
-    elseif doesCurrentCarHaveBomb == 1 and isPedInVehicle == 1 and GetDistanceBetweenCoords(-335.5, -134.08, 39.51, playerLoc.x, playerLoc.y, playerLoc.z) < 12 then
-        ClearVehiclePhoneExplosiveDevice(playerVehicle)
-        exports.pNotify:SendNotification({text = "Your car bomb is removed.", type = "success", timeout = (10000), layout = "bottomCenter",})
-    elseif doesCurrentCarHaveBomb == 1 and isPedInVehicle == 1 and GetDistanceBetweenCoords(-208.78, -1325.7, 30.89, playerLoc.x, playerLoc.y, playerLoc.z) < 12 then
-        ClearVehiclePhoneExplosiveDevice(playerVehicle)
-        exports.pNotify:SendNotification({text = "Your car bomb is removed.", type = "success", timeout = (10000), layout = "bottomCenter",})
-    elseif doesCurrentCarHaveBomb == 1 and isPedInVehicle == 1 and GetDistanceBetweenCoords(-1154.54, -2009.08, 13.18, playerLoc.x, playerLoc.y, playerLoc.z) < 12 then
+    for k, v in pairs(LSC) do
+        if #(playerLoc - v) < 21.0 then
+            inLSC = true
+        end
+    end
+    for k, v in pairs(Beekers) do
+        if #(playerLoc - v) < 12.0 then
+            inBeekers = true
+        end
+    end
+    if #(playerLoc - Bennys) < 15.0 then
+        inBennys = true
+    end
+    
+    if doesCurrentCarHaveBomb == 1 and inLSC or inBeekers or inBennys then
         ClearVehiclePhoneExplosiveDevice(playerVehicle)
         exports.pNotify:SendNotification({text = "Your car bomb is removed.", type = "success", timeout = (10000), layout = "bottomCenter",})
     else
@@ -63,16 +84,16 @@ end)
 RegisterCommand('detonatecarbomb', function()
 
     local getPlayerPed = GetPlayerPed(-1)
-    local playerVehicle = GetVehiclePedIsUsing(getPlayerPed) -- get player vehicle
     local playerLastVehicle = GetVehiclePedIsIn(getPlayerPed, 1)
     local doesCarHaveBomb = HasVehiclePhoneExplosiveDevice(playerLastVehicle)
     local playerLoc = GetEntityCoords(getPlayerPed)
     local vehiclePos = GetEntityCoords(playerLastVehicle)
+    local vehAndPedDist = GetDistanceBetweenCoords(vehiclePos.x, vehiclePos.y, vehiclePos.z, playerLoc.x, playerLoc.y, playerLoc.z) < detonateRadius
     
-    if doesCarHaveBomb == 1 and GetDistanceBetweenCoords(vehiclePos.x, vehiclePos.y, vehiclePos.z, playerLoc.x, playerLoc.y, playerLoc.z) < detonateRadius then
+    if doesCarHaveBomb == 1 and vehAndPedDist then
         DetonateVehiclePhoneExplosiveDevice(playerLastVehicle)
         exports.pNotify:SendNotification({text = "Your car bomb has exploded.", type = "success", timeout = (10000), layout = "bottomCenter",})
-    elseif doesCarHaveBomb == 1 and GetDistanceBetweenCoords(vehiclePos.x, vehiclePos.y, vehiclePos.z, playerLoc.x, playerLoc.y, playerLoc.z) > detonateRadius then
+    elseif doesCarHaveBomb == 1 and not vehAndPedDist then
         exports.pNotify:SendNotification({text = "You are too far away from the vehicle.", type = "error", timeout = (10000), layout = "bottomCenter",})
     else
         exports.pNotify:SendNotification({text = "The vehicle doesn't have a car bomb.", type = "error", timeout = (10000), layout = "bottomCenter",})
